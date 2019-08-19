53813400c4bca4cb1cc185ea3408e7382fea7643
Jackmc <jackmc1122@gmail.com>
2019-08-19 11:55:57 -0700

    Add fetchNotes api method.

--- src/services/notesApi.js
+++ src/services/notesApi.js
@@ -12,3 +12,17 @@ export const createNote = (title, body) => {
       return res.json();
     });
 };
+
+export const fetchNotes = () => {
+  return fetch('http://localhost:7891/api/v1/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => {
+      if(!res.ok) throw 'Could not get notes';
+
+      return res.json();
+    });
+};
