38bb69c3ac527837dc20ceae18d28fad1dfd9043
Jackmc <jackmc1122@gmail.com>
2019-08-19 09:40:26 -0700

    Initialize NoteForm component.

---
+++ src/components/NoteForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NoteForm = () => {
+  return (
+    <h2>Note Form</h2>
+  );
+};
+
+NoteForm.propTypes = {
+
+};
+
+export default NoteForm;
